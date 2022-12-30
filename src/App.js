334b64fb99c7874926cfba18c96b9f8d8cc7500b
Dennis Paskel <dpaskel@gmail.com>
2022-12-30 11:50:34 -0500

    add delete functionality and display message when no feedback exists

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import FeedbackList from "./components/FeedbackList";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
+    
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            let updatedFeedback = feedback.filter(item => item.id !== id);
+            setFeedback(updatedFeedback);
+        }
+    }
 
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
         </>
     );
