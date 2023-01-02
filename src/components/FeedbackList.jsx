e7dbea4128fa5ad48779f2c9aa5311f4be28980a
Dennis Paskel <dpaskel@gmail.com>
2023-01-02 11:39:33 -0500

    add json server for mock backend and update context to use it

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,12 +1,13 @@
 import {motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackItem from "./FeedbackItem";
+import Spinner from './shared/Spinner';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext);
+    const {feedback, isLoading} = useContext(FeedbackContext);
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>
     };
 
@@ -21,13 +22,14 @@ function FeedbackList() {
         </motion.div>
     )
 
-    return (
+    return isLoading ? <Spinner /> : (
         <div className="feedback-list">
             <AnimatePresence>
                 {feedbackList}  
             </AnimatePresence>
         </div>
     )
+
 }
 
 export default FeedbackList;
